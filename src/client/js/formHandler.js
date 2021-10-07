2f3cc6bf5fb854f550bd75741ff61457fe1f484f
albertymq <albertzhouhehe@gmail.com>
2021-10-07 22:07:20 +1100

    Function Done

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,37 @@
-function handleSubmit(event) {
-    event.preventDefault()
-    
-    // check what text was put into the form field
-    let formText = document.getElementById('content').value
-    Client.checkForName(formText)
+import { updateUI } from "..";
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // check what text was put into the form field
+  const formText = document.getElementById("content").value;
+  Client.checkForName(formText);
+
+  console.log("::: Form Submitted :::");
+
+  //getApiKey from Server
+  Client.getApiKey().then(function (data) {
+    console.log("::: getting analysis from Meaning Cloud :::");
+    let apiKey = data.ApiKey;
+    Client.reqToMeaningCloud(apiKey, formText).then(function (data) {
+      Client.postData("http://localhost:8081/addAnalysis", {
+        agreement: data.agreement,
+        confid: data.confidence,
+        irony: data.irony,
+        subjectivity: data.subjectivity,
+        content: formText,
+        remainCredits: data.status.remaining_credits,
+      }).then(function(newdata){
+          updateUI(newdata);
+      });
+    });
+  });
+
+  /*fetch("http://localhost:8081/test")
+    .then((res) => res.json())
+    .then(function (res) {
+      document.getElementById("results").innerHTML = res.message;
+    });*/
 }
 
-export { handleSubmit }
+export { handleSubmit };
