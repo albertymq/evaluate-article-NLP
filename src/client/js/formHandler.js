a8c6bc72d76c5a236830a54135b85a58f4fd4f6b
albertymq <albertzhouhehe@gmail.com>
2021-10-08 11:53:07 +1100

    Updates
    Adding regex condition to check input
    adding service worker to webpack.dev configuration

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,27 +3,30 @@ function handleSubmit(event) {
 
   // check what text was put into the form field
   const formText = document.getElementById("content").value;
+  const regex = /^[\s\S]{10,}$/;
+  if (regex.test(formText)) {
+    console.log("::: Form Submitted :::");
 
-  console.log("::: Form Submitted :::");
-
-  //getApiKey from Server
-  Client.getApiKey().then(function (data) {
-    console.log("::: getting analysis from Meaning Cloud :::");
-    let apiKey = data.ApiKey;
-    Client.reqToMeaningCloud(apiKey, formText).then(function (data) {
-      Client.postData("http://localhost:8081/addAnalysis", {
-        agreement: data.agreement,
-        confid: data.confidence,
-        irony: data.irony,
-        subjectivity: data.subjectivity,
-        content: formText,
-        remainCredits: data.status.remaining_credits,
-      }).then(function(newdata){
+    //getApiKey from Server
+    Client.getApiKey().then(function (data) {
+      console.log("::: getting analysis from Meaning Cloud :::");
+      let apiKey = data.ApiKey;
+      Client.reqToMeaningCloud(apiKey, formText).then(function (data) {
+        Client.postData("http://localhost:8081/addAnalysis", {
+          agreement: data.agreement,
+          confid: data.confidence,
+          irony: data.irony,
+          subjectivity: data.subjectivity,
+          content: formText,
+          remainCredits: data.status.remaining_credits,
+        }).then(function (newdata) {
           Client.updateUI(newdata);
+        });
       });
     });
-  });
-
+  } else {
+    alert("Please input a sentence more than 10 characters");
+  }
   /*fetch("http://localhost:8081/test")
     .then((res) => res.json())
     .then(function (res) {
